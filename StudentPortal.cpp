880eb55e98965e00bd07b09f2c51b853079f453c
Samia-100Bit <samiaafroze6@gmail.com>
2026-08-11 15:34:29 +0600

    feat: printing age in Student structure

--- StudentPortal.cpp
+++ StudentPortal.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 struct Student
 {
     int id;
@@ -5,3 +8,25 @@ struct Student
     int age;
     float cgpa;
 };
+
+void displayStudents(Student students[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << students[i].age << endl;
+    }
+}
+
+int main()
+{
+    Student students[3] =
+    {
+        {1, "Samia", 20, 4.00},
+        {2, "Rahim", 21, 3.75},
+        {3, "Karim", 22, 3.50}
+    };
+
+    displayStudents(students, 3);
+
+    return 0;
+}
